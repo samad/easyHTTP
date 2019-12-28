58c9fd747899a2c77cf7013ae3c81dec7013cba1
Abdul Samad <samk8778@yahoo.com>
2019-12-28 17:14:00 +0530

    :bookmark: v3.0.0

--- easyHTTP.js
+++ easyHTTP.js
@@ -1,54 +1,42 @@
 class easyHTTP {
-	get(url) {
-		return new Promise((resolve, reject) => {
-			fetch(url)
-				.then(res => res.json())
-				.then(data => resolve(data))
-				.catch(err => reject(err));
-		});
+	async get(url) {
+		const response = await fetch(url);
+		const resData = await response.json();
+		return resData;
 	}
 
-	post(url, data) {
-		return new Promise((resolve, reject) => {
-			fetch(url, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data),
-			})
-				.then(res => res.json())
-				.then(data => resolve(data))
-				.catch(err => reject(err));
+	async post(url, data) {
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
 		});
+		const resData = await response.json();
+		return resData;
 	}
 
-	put(url, data) {
-		return new Promise((resolve, reject) => {
-			fetch(url, {
-				method: 'PUT',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(data),
-			})
-				.then(res => res.json())
-				.then(data => resolve(data))
-				.catch(err => reject(err));
+	async put(url, data) {
+		const response = await fetch(url, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
 		});
+		const resData = await response.json();
+		return resData;
 	}
 
-	delete(url) {
-		return new Promise((resolve, reject) => {
-			fetch(url, {
-				method: 'DELETE',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			})
-				.then(res => res.json())
-				.then(() => resolve('Resource Deleted'))
-				.catch(err => reject(err));
+	async delete(url) {
+		const response = await fetch(url, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
 		});
+		const resData = await 'Resource Deleted';
+		return resData;
 	}
 }
