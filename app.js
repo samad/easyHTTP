57a63d03fab20dc977c7df24091777b4af036250
Abdul Samad <samk8778@yahoo.com>
2019-12-28 16:53:19 +0530

    :bookmark: v2.0.0

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var data = {
 
 var http = new easyHTTP();
 
+/* XHR */
+
 // GET
 // http.get('https://jsonplaceholder.typicode.com/posts/10', function(err, res) {
 // 	console.log(res);
@@ -24,3 +26,21 @@ var http = new easyHTTP();
 // http.get('https://jsonplaceholder.typicode.com/posts/10', function(err, res) {
 // 	console.log(res);
 // });
+
+/* Promise */
+
+// GET
+// const res = http.get('https://jsonplaceholder.typicode.com/users/');
+// res.then(res => console.log(res));
+
+// POST
+// const res = http.post('https://jsonplaceholder.typicode.com/users/', data);
+// res.then(res => console.log(res));
+
+// PUT
+// const res = http.put('https://jsonplaceholder.typicode.com/users/2', data);
+// res.then(res => console.log(res));
+
+// DELETE
+// const res = http.delete('https://jsonplaceholder.typicode.com/users/2');
+// res.then(res => console.log(res));
--- easyHTTP.js
+++ easyHTTP.js
@@ -1,69 +1,54 @@
-function easyHTTP() {
-	this.http = new XMLHttpRequest();
+class easyHTTP {
+	get(url) {
+		return new Promise((resolve, reject) => {
+			fetch(url)
+				.then(res => res.json())
+				.then(data => resolve(data))
+				.catch(err => reject(err));
+		});
+	}
+
+	post(url, data) {
+		return new Promise((resolve, reject) => {
+			fetch(url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			})
+				.then(res => res.json())
+				.then(data => resolve(data))
+				.catch(err => reject(err));
+		});
+	}
+
+	put(url, data) {
+		return new Promise((resolve, reject) => {
+			fetch(url, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(data),
+			})
+				.then(res => res.json())
+				.then(data => resolve(data))
+				.catch(err => reject(err));
+		});
+	}
+
+	delete(url) {
+		return new Promise((resolve, reject) => {
+			fetch(url, {
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+				.then(res => res.json())
+				.then(() => resolve('Resource Deleted'))
+				.catch(err => reject(err));
+		});
+	}
 }
-
-// GET
-easyHTTP.prototype.get = function(url, callback) {
-	this.http.open('GET', url, true);
-	var self = this;
-
-	this.http.onload = function() {
-		if (self.http.status === 200) {
-			callback(null, self.http.responseText);
-		} else {
-			callback('Error: ' + self.http.status);
-		}
-	};
-
-	this.http.send();
-};
-
-// POST
-easyHTTP.prototype.post = function(url, data, callback) {
-	this.http.open('POST', url, true);
-	this.http.setRequestHeader('Content-type', 'application/json');
-	var self = this;
-
-	this.http.onload = function() {
-		if (self.http.status >= 200 && self.http.status <= 210) {
-			callback(null, self.http.responseText);
-		} else {
-			callback('Error: ' + self.http.status);
-		}
-	};
-
-	this.http.send(JSON.stringify(data));
-};
-
-// PUT
-easyHTTP.prototype.put = function(url, data, callback) {
-	this.http.open('PUT', url, true);
-	this.http.setRequestHeader('Content-type', 'application/json');
-	var self = this;
-
-	this.http.onload = function() {
-		if (self.http.status >= 200 && self.http.status <= 210) {
-			callback(null, self.http.responseText);
-		} else {
-			callback('Error: ' + self.http.status);
-		}
-	};
-
-	this.http.send(JSON.stringify(data));
-};
-
-// DELETE
-easyHTTP.prototype.delete = function(url, callback) {
-	this.http.open('DELETE', url, true);
-	var self = this;
-
-	this.http.onload = function() {
-		if (self.http.status === 200) {
-			callback(null, 'Deleted');
-		} else {
-			callback('Error: ' + self.http.status);
-		}
-	};
-
-	this.http.send();
-};
